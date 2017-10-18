537ab93baae4e796e29c82af5fdc3459ce4bc0cb
MGApcDev <mgapcdev@gmail.com>
2017-10-19 00:04:31 +0200

    Implement parser

--- src/main.py
+++ src/main.py
@@ -12,12 +12,12 @@ class LetterBranch(object):
         remain_dict Object.<char, int> The remaining letters of the phrase from this point in the tree
         used_dict   Object.<char, int> The used letters of the phrase for getting to this point in the tree
     """
-    def __init__(self,  letter, is_word, origin, remain_dict, used_dict):
+    def __init__(self,  letter, is_word, origin, remain_dict, children):
         self.letter = letter
         self.is_word = is_word
         self.origin = origin
         self.remain_dict = remain_dict
-        self.used_dict = used_dict
+        self.children = children
 
 class WordBranch(object):
     """WordBranch represents a single branch in the tree of all the valid word combinations.
@@ -31,6 +31,50 @@ class WordBranch(object):
         self.cur_remain_dict = cur_remain_dict
 
 '''Functions'''
+
+
+def invalid_char(char):
+    if char == '\n' or char == ' ':
+        return True
+
+    return False
+
+def append_word_to_tree(root, word, remain_dict):
+    pointer = root
+    last_char = None
+
+
+    valid_word = True
+    # Check the word matches the remaining characters.
+    for char in word:
+        if invalid_char(char): # Ignore newlines
+            continue
+
+        if not(char in remain_dict):
+            valid_word = False
+            break
+        if remain_dict[char] <= 0:
+            valid_word = False
+            break
+        remain_dict[char] -= 1
+
+    if not(valid_word):
+        return False
+
+    # Append valid word to tree.
+    for char in word:
+        if invalid_char(char): # Ignore newlines
+            continue
+        last_char = char
+
+        if not(char in pointer.children):
+            pointer.children[char] = LetterBranch(char, False, pointer, {}, {})
+        pointer = pointer.children[char]
+
+    pointer.is_word = True
+    pointer.remain_dict = remain_dict
+
+    return True
 
 '''Convert string phrase to dictionary <char, int>, mapping letters to a count of them found in a given phrase'''
 def phrase_to_dict(phrase):
@@ -46,7 +90,19 @@ def phrase_to_dict(phrase):
 
 def parse_words(phrase, filename):
     phrase_dict = phrase_to_dict(phrase)
-    pass
+    root = LetterBranch(None, False, None, phrase_dict, {})
+
+    for line in open(filename):
+        remain_dict = dict(phrase_dict) # Make copy of dictionary without reference
+        append_word_to_tree(root, line, remain_dict)
+
+    return root
+
+def get_word(letter_branch):
+    str = ''
+    pointer = letter_branch
+    while pointer.origin != None:
+        pass
 
 def append_to_solutions(branch_obj):
     pass
@@ -64,8 +120,8 @@ def output(solutions):
 if __name__ == "__main__":
     args = sys.argv
     if len(args) == 3:
-        phrase = args[0]
-        wordlist_filename = args[1]
+        phrase = args[1]
+        wordlist_filename = args[2]
 
         valid_words = parse_words(phrase, wordlist_filename)
         candidates = construct_tree(phrase, valid_words)
