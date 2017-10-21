290e05feaaef3cda3d317faf6b0128382371d811
Mathias Grundtvig Andreasen <mgapcdev@gmail.com>
2017-10-21 02:03:07 +0200

    Implement parser (#2)
    * Implement phrase_to_dict
    
    * Add docs to phrase_to_dict
    
    * Implement parser
    
    * Implement get_word
    
    * Decide on docstring style and move dependency
    
    * Update style

--- src/main.py
+++ src/main.py
@@ -6,42 +6,149 @@ class LetterBranch(object):
     """LetterBranch represents a single branch in the tree of all the words in the loaded dictionary.
 
     Attributes:
-        letter      string             The letter for the place in the tree
-        is_word     bool               Whether there is a word ending in the LetterBranch object
-        origin      LetterBranch       The reference to the parent LetterBranch
-        remain_dict Object.<char, int> The remaining letters of the phrase from this point in the tree
-        used_dict   Object.<char, int> The used letters of the phrase for getting to this point in the tree
+        letter      (string)             The letter for the place in the tree.
+        is_word     (bool)               Whether there is a word ending in the LetterBranch object.
+        origin      (LetterBranch)       The reference to the parent LetterBranch.
+        remain_dict (Object.<char, int>) The remaining letters of the phrase from this point in the tree.
+        used_dict   (Object.<char, int>) The used letters of the phrase for getting to this point in the tree.
     """
-    def __init__(self,  letter, is_word, origin, remain_dict, used_dict):
+    def __init__(self,  letter, is_word, origin, remain_dict, children):
         self.letter = letter
         self.is_word = is_word
         self.origin = origin
         self.remain_dict = remain_dict
-        self.used_dict = used_dict
+        self.children = children
 
 class WordBranch(object):
     """WordBranch represents a single branch in the tree of all the valid word combinations.
 
     Attributes:
-        letter_branch   LetterBranch       The reference to the LetterBranch that represents the word
-        cur_remain_dict Object.<char, int> The remaining letters of the phrase from this point in the tree
+        letter_branch   (LetterBranch)       The reference to the LetterBranch that represents the word.
+        cur_remain_dict (Object.<char, int>) The remaining letters of the phrase from this point in the tree.
     """
     def __init__(self,  letter_branch, cur_remain_dict):
         self.letter_branch = letter_branch
         self.cur_remain_dict = cur_remain_dict
 
 '''Functions'''
+
+def invalid_char(char):
+    '''Check if character is invalid.
+
+    Args
+        char (string) The character to check.
+    Returns
+        (bool) The True if char should be ignored otherwise return False.
+    '''
+    if char == '\n' or char == ' ':
+        return True
+
+    return False
+
+def append_word_to_tree(root, word, remain_dict_ref):
+    '''Append word to abstract syntax tree.
+
+    Args
+        root            (LetterBranch)       The root of the syntax tree.
+        word            (string)             The word to add.
+        remain_dict_ref (Object.<char, int>) The dictionary of the remaining available characters.
+    Returns
+        (bool) Return True if word was added otherwise return False.
+    '''
+    remain_dict = dict(remain_dict_ref) # Make copy of dictionary without reference
+    pointer = root
+    last_char = None
+
+    valid_word = True
+    # Check the word matches the remaining characters.
+    for char in word:
+        if invalid_char(char): # Ignore newlines
+            continue
+
+        if not(char in remain_dict):
+            valid_word = False
+            break
+        if remain_dict[char] <= 0:
+            valid_word = False
+            break
+        remain_dict[char] -= 1
+
+    if not(valid_word):
+        return False
+
+    # Append valid word to tree.
+    for char in word:
+        if invalid_char(char): # Ignore newlines
+            continue
+        last_char = char
+
+        if not(char in pointer.children):
+            pointer.children[char] = LetterBranch(char, False, pointer, {}, {})
+        pointer = pointer.children[char]
+
+    pointer.is_word = True
+    pointer.remain_dict = remain_dict
+
+    return True
+
 def phrase_to_dict(phrase):
-    pass
+    '''Convert string phrase to dictionary <char, int>, mapping letters to a count of them found in a given phrase.
+
+    Args
+        phrase (string) The string phrase to convert.
+    Returns
+        (Object.<char, int>) Returns a dictionary of characters and a count of their appearances in the phrase.
+    '''
+    phrase_dict = {}
+    for char in phrase:
+        if char == " ": # ignore spaces
+            continue
+        if char in phrase_dict:
+            phrase_dict[char] += 1
+        else:
+            phrase_dict[char] = 1
+    return phrase_dict
 
 def parse_words(phrase, filename):
+    '''Parse file to abstract syntax tree.
+
+    Args
+        phrase   (string) The phrase to look for anagrams.
+        filename (string) The filename of the list of available words.
+    Returns
+        (LetterBranch) The root of the abstract syntax tree.
+    '''
     phrase_dict = phrase_to_dict(phrase)
-    pass
+    root = LetterBranch(None, False, None, phrase_dict, {})
+
+    for line in open(filename):
+        append_word_to_tree(root, line, phrase_dict)
+        ###
+        # Create first level of word-tree
+        ###
+
+    return root
+
+def get_word(letter_branch):
+    '''Trace word from leaf branch to root.
+
+    Args
+        letter_branch (LetterBranch) The leaf branch to trace for word.
+    Returns
+        (string) The full string of represented by the leaf.
+    '''
+    word_str = ''
+    pointer = letter_branch
+    while pointer.origin != None:
+        word_str += pointer.letter
+        pointer = pointer.origin
+
+    return word_str[::-1] # Flip string
 
 def append_to_solutions(branch_obj):
     pass
 
-def construct_tree(phrase, valid_words):
+def construct_tree(phrase, letter_tree):
     pass
 
 def find_solutions(candidates):
@@ -54,11 +161,11 @@ def output(solutions):
 if __name__ == "__main__":
     args = sys.argv
     if len(args) == 3:
-        phrase = args[0]
-        wordlist_filename = args[1]
+        phrase = args[1]
+        wordlist_filename = args[2]
 
-        valid_words = parse_words(phrase, wordlist_filename)
-        candidates = construct_tree(phrase, valid_words)
+        letter_tree = parse_words(phrase, wordlist_filename)
+        candidates = construct_tree(phrase, letter_tree)
 
         solutions = find_solutions(candidates)
         output(solutions)
--- src/test/parse-test.py
+++ src/test/parse-test.py
@@ -1,4 +1,77 @@
 from main import *
 
-def test_travis():
-    assert 1 == 1
+def test_phrase_to_dict_simple():
+    phrase_dict = phrase_to_dict("abc")
+    assert len(phrase_dict.keys()) == 3
+    assert phrase_dict['a'] == 1
+    assert phrase_dict['c'] == 1
+    assert ("d" in phrase_dict) == False
+
+def test_phrase_to_dict_repeat_and_multiple_words_and_distinct_case():
+    phrase_dict = phrase_to_dict("apple of Eden")
+    assert len(phrase_dict.keys()) == 9
+    assert phrase_dict['a'] == 1
+    assert phrase_dict['p'] == 2
+    assert phrase_dict['e'] == 2
+    assert phrase_dict['E'] == 1
+    assert (" " in phrase_dict) == False # ignore spaces
+
+def test_append_word_to_tree_single():
+    phrase_dict = phrase_to_dict("apple of Eden")
+    root = LetterBranch(None, False, None, phrase_dict, {})
+    append_word_to_tree(root, "pale\n", phrase_dict)
+
+    assert root.children['p'] != None
+    assert root.children['p'].children['a'] != None
+    assert root.children['p'].children['a'].children['l'] != None
+    assert root.children['p'].children['a'].children['l'].children['e'] != None
+
+def test_append_word_to_tree_multiple():
+    phrase_dict = phrase_to_dict("apple of Edenmot kaes")
+    root = LetterBranch(None, False, None, phrase_dict, {})
+
+    remain_dict = phrase_dict
+
+    ret = append_word_to_tree(root, "pale\n", remain_dict)
+    assert ret == True # Check that word was added
+
+    ret = append_word_to_tree(root, "pakes\n", remain_dict)
+    assert ret == True
+
+    ret = append_word_to_tree(root, "tom\n", remain_dict)
+    assert ret == True
+
+    ret = append_word_to_tree(root, "applew", remain_dict) # Invalid word
+    assert ret == False
+
+    # Check tree structure
+    assert root.children['p'] != None
+    assert root.children['p'].children['a'] != None
+    assert root.children['p'].children['a'].children['l'] != None
+    assert root.children['p'].children['a'].children['l'].children['e'] != None
+
+    assert root.children['p'].children['a'].children['k'] != None
+    assert root.children['p'].children['a'].children['k'].children['e'] != None
+    assert root.children['p'].children['a'].children['k'].children['e'].children['s'] != None
+    assert root.children['p'].children['a'].children['k'].children['e'].children['s'].is_word == True
+
+    assert root.children['t'] != None
+    assert root.children['t'].children['o'] != None
+    assert root.children['t'].children['o'].children['m'] != None
+
+    # Check is_word
+    assert root.children['p'].children['a'].children['l'].is_word == False
+    assert root.children['p'].children['a'].children['l'].children['e'].is_word == True
+
+    # Check local remain_dict
+    assert root.children['p'].children['a'].children['k'].children['e'].children['s'].remain_dict['a'] == 1
+    assert root.children['p'].children['a'].children['k'].children['e'].children['s'].remain_dict['p'] == 1
+    assert root.children['p'].children['a'].children['k'].children['e'].children['s'].remain_dict['k'] == 0
+    assert root.children['p'].children['a'].children['k'].children['e'].children['s'].remain_dict['E'] == 1
+
+def test_parse_word():
+    tree = parse_words("poultry outwits ants", "data/wordlist")
+
+    # Exclude invalid words.
+    assert ('z' in tree.children) == False
+    tree.children['t'].children['a'].children['i'].children['l'].is_word == True
